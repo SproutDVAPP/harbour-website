fc5ec2a5f9a4159dfa190035701873f12b96df0f
KG <kartikg9@gmail.com>
2024-04-19 22:31:40 -0400

    Initial commit

--- script.js
+++ script.js
@@ -2,17 +2,4 @@ function quickExit() {
     window.location.href = "https://www.google.com";
 }
 
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Your message has been sent. We will contact you shortly.');
-});
-
-// Smooth scroll for anchor links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+document.getElementById('quick-exit').addEventListener('click', quickExit);
